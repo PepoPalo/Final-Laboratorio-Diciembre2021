180369851da14960deddffa63d040369a545f4d2
Milagros Vazquez <milivazquezjeje@gmail.com>
2021-11-30 15:20:21 -0300

    new

--- Backend/dominio/alumnomateria.py
+++ Backend/dominio/alumnomateria.py
@@ -2,11 +2,17 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String, Numeric, Date, Float
 from datos import db
 
-class Lineaequipoplan(db.Model):
+class AlumnoMateria(db.Model):
     __tablename__ = 'alumno_materia'
-    # id = Column(Integer(), primary_key=True, autoincrement=True)
-    alumno_id = Column(Integer(), nullable=False)
-    curso_id = Column(Integer(), nullable=False)
-    ForeignKeyConstraint(['alumno_id'], ['alumno.id'])
-    ForeignKeyConstraint(['curso_id'], ['curso.id'])
+    __table_args__ = (
+        db.UniqueConstraint('curso_id', 'alumno_id', name='unique_curso_alumno'),
+    )
+    id = Column(Integer(), primary_key=True, autoincrement=True)
+    curso_id = Column(Integer, ForeignKeu('curso.id'))
+    alumno_id = Column(Integer,  ForeignKey('alumno.id'))
+    curso = relationship("Curso", backref="alumno_materia",lazy='joined')
+    alumno = relationship("Alumno", backref="alumno_materia",lazy='joined')
+
+    # ForeignKeyConstraint(['alumno_id'], ['alumno.id'])
+    # ForeignKeyConstraint(['curso_id'], ['curso.id'])
 
